599abd82cfc74d297cdbf66146ceab2b96c60e48
silvertejp89 <kriss.stockhaus@gmail.com>
2021-10-30 16:40:29 +0200

    FInished blue level, styled.

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -3,15 +3,28 @@ import React from "react";
 const ThoughtForm = ({ onFormSubmit, newThought, setNewThought }) => {
   return (
     <form className="form" onSubmit={onFormSubmit}>
-      <label htmlFor="newThought">Type your thought</label>
-      <input
+      <label htmlFor="newThought" className="input-label">
+        <p>What´s making you happy right now?</p>
+      </label>
+      <textarea
+        className="text-input"
         id="newThought"
         type="text"
         value={newThought}
         onChange={(e) => setNewThought(e.target.value)}
       />
-      <button disabled={newThought.length < 5} type="submit">
-        Send thought
+      <button
+        disabled={newThought.length < 5}
+        type="submit"
+        className="send-button"
+      >
+        <span className="heart" role="img" aria-label="heart emoji">
+          ❤️
+        </span>
+        Send Happy Thought!
+        <span className="heart" role="img" aria-label="heart emoji">
+          ❤️
+        </span>
       </button>
     </form>
   );
--- code/src/components/ThoughtItem.js
+++ code/src/components/ThoughtItem.js
@@ -3,15 +3,26 @@ import moment from "moment";
 
 const ThoughtItem = ({ thought, onLikesIncrease }) => {
   return (
-    <div className="thought-item">
-      <p>{thought.message}</p>
-      <button onClick={() => onLikesIncrease(thought._id)}>
-        &hearts; {thought.hearts}
-      </button>
-      <p className="date">
-        - Created at: {moment(thought.createdAt).fromNow()}
-      </p>
-    </div>
+    <section className="thought-item">
+      <p className="message">{thought.message}</p>
+      <div className="likes-date">
+        <div className="likes">
+          <button
+            className="like-button"
+            onClick={() => onLikesIncrease(thought._id)}
+          >
+            <span role="img" aria-label="heart emoji">
+              ❤️
+            </span>
+          </button>
+
+          <span className="total-likes">
+            <p>x {thought.hearts}</p>
+          </span>
+        </div>
+        <p className="date">{moment(thought.createdAt).fromNow()}</p>
+      </div>
+    </section>
   );
 };
 
