128a6e60291718df4b1a9192eb1278fd3a6dd1b9
silvertejp89 <kriss.stockhaus@gmail.com>
2022-01-07 15:49:13 +0100

    spelling

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://krisshappythoughts.herokuapp.com/thougths";
+export const API_URL = "https://krisshappythoughts.herokuapp.com/thoughts";
 export const LIKES_URL = (thoughtId) =>
   `https://krisshappythoughts.herokuapp.com/thoughts/${thoughtId}/hearts`;
